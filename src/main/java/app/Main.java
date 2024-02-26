2f6ce5a0b83cac175c8c553306945789ca398ecb
Nazar Demchuk <nazar.demchuk@lynxtrading.com>
2024-02-26 13:25:32 +0200

    Distributed map example

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,20 +9,16 @@ public class Main {
         Config config = new Config();
         config.setClusterName("my-hazelcast-cluster");
 
-        // starting three nodes
         HazelcastInstance instance1 = Hazelcast.newHazelcastInstance(config);
         HazelcastInstance instance2 = Hazelcast.newHazelcastInstance(config);
         HazelcastInstance instance3 = Hazelcast.newHazelcastInstance(config);
 
         System.out.println("Three Hazelcast instances have started in a single cluster.");
 
-        // crating a map on the first node
         var distributedMap = instance1.getMap("my-distributed-map");
-        // pushing a value to a map
-        distributedMap.put("key", "value");
+        for (int i = 0; i != 1000; i++) {
+            distributedMap.put(i, String.valueOf(i));
+        }
 
-        // comparing the values between nodes
-        System.out.println("Value from instance2: " + instance2.getMap("my-distributed-map").get("key"));
-        System.out.println("Value from instance3: " + instance3.getMap("my-distributed-map").get("key"));
     }
 }
